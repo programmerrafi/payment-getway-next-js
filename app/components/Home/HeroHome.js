360b62d643fa949429f3e016e36b5d56a62e6753
programmerrafi <mdrafiprogrammer@gmail.com>
2022-12-07 22:25:25 +0600

    Hero section almost done

--- app/components/Home/HeroHome.js
+++ app/components/Home/HeroHome.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { setBackgroundImage } from "../utils/helpers";
 
 function HeroHome() {
+  const [showText, setShowText] = useState(false);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => setShowText(!showText), [2200]);
+    return () => clearInterval(intervalId);
+  }, [showText]);
+
   return (
-    <section className="relative rounded-br-[300px] mix-transition overflow-hidden mb-0 -mt-[95px] pt-[200px] pb-[120px] hero-gradient z-0">
+    <section className="relative rounded-br-[300px] mix-transition overflow-hidden mb-0 -mt-[95px] pt-[230px] pb-[120px] hero-gradient z-0">
       <div
         className="w-full h-full absolute top-0 left-0 mix-transition"
         style={setBackgroundImage("images/hero-bg-img.jpeg", {
@@ -12,7 +19,38 @@ function HeroHome() {
         })}
       ></div>
       <div className="relative container">
-        <h1>Hello Hero sections</h1>
+        <div className="flex flex-warp lg:flex-nowrap">
+          <div className="lg:w-[55%]">
+            <h1 className="text-[55px] text-white font-RadHatText font-thin leading-[75px] transition-all">
+              <span>Everpay </span>
+              <span className="font-medium relative inline-block w-[120px] duration-300 ease-linear align-bottom">
+                <span
+                  className={`dynamic-text ${
+                    showText ? "show-text" : "hide-text"
+                  }`}
+                >
+                  Easy{" "}
+                </span>
+                <span
+                  className={`dynamic-text ${
+                    showText ? "hide-text" : "show-text"
+                  }`}
+                >
+                  Fast{" "}
+                </span>
+              </span>
+              <span
+                className={`relative z-100 ${
+                  !showText && "-ml-2 transition-all"
+                }`}
+              >
+                {" "}
+                Payment Gateway Solution
+              </span>
+            </h1>
+          </div>
+          <div className="lg:w-[45%]">Hero Project Image</div>
+        </div>
       </div>
     </section>
   );
