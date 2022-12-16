3c0278b1f309fbca8826ec45f1ce3f357700b085
programmerrafi <mdrafiprogrammer@gmail.com>
2022-12-16 22:12:44 +0600

    no working today

--- app/components/Home/EasyPayment.js
+++ app/components/Home/EasyPayment.js
@@ -8,6 +8,7 @@ import ButtonC from "../shared/ButtonC";
 import PaymentCard from "../Cart/PaymentCard";
 
 function EasyPayment() {
+  console.log("No working today");
   return (
     <section className="relative">
       <div className="flex flex-wrap lg:flex-nowrap">
