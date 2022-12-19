d66bf6d1ca195dbc22a8956f2c3131316545b20e
programmerrafi <mdrafiprogrammer@gmail.com>
2022-12-19 23:35:21 +0600

    No update available

--- app/components/Home/EasyPayment.js
+++ app/components/Home/EasyPayment.js
@@ -8,8 +8,7 @@ import ButtonC from "../shared/ButtonC";
 import PaymentCard from "../Cart/PaymentCard";
 
 function EasyPayment() {
-  console.log("No working today");
-
+  console.log("No Update Available");
   return (
     <section className="relative">
       <div className="flex flex-wrap lg:flex-nowrap">
